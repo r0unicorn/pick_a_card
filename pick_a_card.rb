c16ff4f920985618e720d4327860d7aecb75a881
Viktor Kunavin <r0runicorn@gmail.com>
2020-09-01 23:14:39 +0300

    Change to unicode symbol output

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suits = %w[Diamonds Hearts Clubs Spades]
+# suits = %w[Diamonds Hearts Clubs Spades]
+suits = %W[\u2666 \u2665 \u2663 \u2660]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
